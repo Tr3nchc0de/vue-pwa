aa0a120a9b81ebd29c76645f9f2a7e5d990ab03c
Caspar <casparheinemann@gmail.com>
2021-02-19 20:46:13 +0100

    asdf

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.c049b8c45de19a0259dc82906de1eaf2.js");
+
+
 // vue.config.js
 module.exports = {
     // ...other vue-cli plugin options...
